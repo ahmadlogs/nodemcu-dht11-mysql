d6e86d2eba6247e33968d5674b50303488dccfef
ahmadlogs <73652235+ahmadlogs@users.noreply.github.com>
2022-01-21 19:55:22 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php 
-define('DB_HOST'	, 'ENTER_DATABASE_HOST'); 
+define('DB_HOST'    , 'ENTER_DATABASE_HOST'); 
 define('DB_USERNAME', 'ENTER_USERNAME'); 
 define('DB_PASSWORD', 'ENTER_PASSWORD'); 
-define('DB_NAME'	, 'ENTER_DATABASE_NAME');
+define('DB_NAME'    , 'ENTER_DATABASE_NAME');
 
 define('POST_DATA_URL', 'ENTER_YOUR_DOMAIN_NAME/sensordata.php');
 
-//PROJECT_API_KEY is the exact duplicate of, PROJECT_API_KEY in ESP32 sketch file
+//PROJECT_API_KEY is the exact duplicate of, PROJECT_API_KEY in NodeMCU sketch file
 //Both values must be same
 define('PROJECT_API_KEY', 'WHATEVER_YOU_LIKE');
 
